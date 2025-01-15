dd001b1ad61a616f43a1e38041e1c8cec9bd7e44
Alexandr <jozeyts@gmail.com>
2025-01-15 20:36:06 +0300

    s2

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,9 @@
 <script setup>
-import { CheckCircleIcon } from '@heroicons/vue/24/solid'
-import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import TheHeader from './components/TheHeader.vue'
+import TheNav from './components/TheNav.vue';
 </script>
 <template>
-  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
-    <a href="#">
-      <img src="/src/assets/logo.png" alt="logo" class="h-9" />
-    </a>
-    <a href="#" class="text-sm">
-      <div v-if="true" class="flex items-center gap-1">
-        Complete!
-        <CheckCircleIcon class="h-7 text-green-500" />
-      </div>
-      <div v-else class="flex items-center gap-1">
-        <div>Progress: <span class="font-mono">20%</span></div>
-        <div class="h-3 w-3 rounded-full bg-red-500"></div>
-      </div>
-    </a>
-  </header>
+  <TheHeader />
   <main class="flex flex-grow flex-col"></main>
-  <nav class="sticky bottom-0 z-10 bg-white">
-    <ul class="flex items-center justify-around border-t">
-      <li class="flex-1">
-        <a class="flex flex-col items-center p-2 text-xs capitalize" href="#timeline">
-          <ClockIcon class="h-6 w-6" /> Timeline
-        </a>
-      </li>
-      <li class="flex-1">
-        <a class="flex flex-col items-center p-2 text-xs capitalize" href="#activities">
-          <ListBulletIcon class="h-6 w-6" /> Activities
-        </a>
-      </li>
-      <li class="flex-1">
-        <a class="flex flex-col items-center p-2 text-xs capitalize" href="#progress">
-          <ChartBarIcon class="h-6 w-6" /> Progress
-        </a>
-      </li>
-    </ul>
-  </nav>
+  <TheNav />
 </template>
